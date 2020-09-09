2d0b9252938a70a09df0917105462f046016847a
pedro79 <pecepla79@gmail.com>
2020-09-09 11:50:31 +0200

    ratting acabado

--- Rating/js/rating.js
+++ Rating/js/rating.js
@@ -1,5 +1,6 @@
 const start=document.querySelector(".ratting").children;
 
+let contador;
 
 for(let i=0;i<start.length;i++){
 
@@ -9,11 +10,41 @@ for(let j=0;j<start.length;j++){
 start[j].classList.remove("fa-star");
 start[j].classList.add("fa-star-o");
 }
-for(let j=0;j<=i;j++){
-  start[j].classList.remove("fa-star-o");
-  start[j].classList.add("fa-star");
-
+for(let y=0;y<=i;y++){
+  start[y].classList.remove("fa-star-o");
+  start[y].classList.add("fa-star");
+  
   }
-  })
+  });
+  
+  start[i].addEventListener("mouseout", function(){
+    for(let j=0;j<start.length;j++){
+      start[j].classList.remove("fa-star");
+      start[j].classList.add("fa-star-o");
+      }
+      for(let y=0;y<=i;y++){
+        start[y].classList.remove("fa-star-o");
+        start[y].classList.add("fa-star");
+         
+        }}); 
+
+window.addEventListener("click", function(e) { 
+        
+ if(!document.getElementById("ratting").contains(e.target)) { 
  
+  for(let j=0;j<start.length;j++){
+    start[j].classList.remove("fa-star");
+   
+    }
+    for(let y=0;y<=i;y++){
+     
+      start[y].classList.add("fa-star-o");
+       
+    }
+  
+ 
+
+ }
+});
+
 }
